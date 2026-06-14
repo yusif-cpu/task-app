23c739fc2345716959708025b385e064794ea5f8
yusif-cpu <yusif@migration-pro.com>
2026-06-14 20:14:31 +0400

    Initial Laravel Task Management API

--- task-app/app/Http/Controllers/AuthController.php
+++ task-app/app/Http/Controllers/AuthController.php
@@ -52,4 +52,13 @@ class AuthController extends Controller
             'user' => $user
         ]);
     }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
 }
--- task-app/routes/api.php
+++ task-app/routes/api.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Belong to Tasks
     Route::get('/tasks', [TaskController::class, 'index']);
     Route::get('/tasks/{id}', [TaskController::class, 'show']);
     Route::put('/tasks/{id}', [TaskController::class, 'update']);
